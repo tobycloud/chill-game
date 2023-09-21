3df5d200973f89e7baefb1c53d2a12a1b837a326
code ngu nhất quả đất <72681678+code-ga@users.noreply.github.com>
2023-09-21 23:26:57 +0700

    init commit

---
+++ scripts/player.gd
@@ -0,0 +1,83 @@
+extends CharacterBody3D
+
+@onready var third = $camera_mount/third
+@onready var first = $camera_mount/first
+@onready var camera_mount = $camera_mount
+@onready var animation_player = $visuals/mixamo_base/AnimationPlayer
+@onready var visuals = $visuals
+
+
+@export var sens_horizontal = 0.5
+@export var sens_vertical = 0.5
+
+
+var SPEED = 3
+const JUMP_VELOCITY = 5
+var running = false
+
+@export var walking_speed = 3
+@export var running_speed = 5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	if animation_player.current_animation != "idle":
+		animation_player.play("idle")
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if Input.is_action_just_pressed("switch_cam"):
+		if third.current:
+			first.current = true
+			third.current = false
+		else:
+			first.current = false
+			third.current = true
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
+		visuals.rotate_y(deg_to_rad(event.relative.x * sens_horizontal))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y*sens_vertical))
+		
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("run"):
+		SPEED = running_speed
+		running = true
+	else:
+		SPEED = walking_speed
+		running = false
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		if running:
+			if animation_player.current_animation != "running":
+				animation_player.play("running")
+		else:
+			if animation_player.current_animation != "walking":
+				animation_player.play("walking")
+		
+		visuals.look_at(position + direction)
+		
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		if animation_player.current_animation != "idle":
+			animation_player.play("idle")
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
