aa3f21223648529be4e0556ad611d826b162be9c
VN Fosly <Admin@Admin.(none)>
2023-10-17 12:30:34 +0700

    camera movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -17,18 +17,37 @@ var running = false
 var is_locked = false
 var is_locked_cam = false
 var was_emit_die = false
+var lerp_spd = 10.0
+
+var direction = Vector3.ZERO
 
 @export var walking_speed = 3
 @export var running_speed = 5
 
+@export_category("Nodes")
+@export_node_path("Node3D") var node_path
+
+@onready var model = get_node(node_path) as Node3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var target_rotation = Vector2()
+var rotation_speed = 0.1
+var z_rotation_decay = 0.02
+var z_rotation_speed = 0.05
+var z_rotation_deceleration = 0.04
+var z_rotation_acceleration = 0
+var max_z_rotation_speed = 0.5
+var current_z_rotation_speed = 0.0
+var locked = false
+
+var prev_target_rotation = Vector2()
 
 func _ready():
 	if animation_player.current_animation != "idle":
 		animation_player.play("idle")
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	model.visible = false
 
 func _input(event):
 	if Input.is_action_just_pressed("switch_cam"):
@@ -42,12 +61,18 @@ func _input(event):
 		if !is_locked_cam:
 			rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
 			visuals.rotate_y(deg_to_rad(event.relative.x * sens_horizontal))
+		
 			camera_mount.rotate_x(deg_to_rad(-event.relative.y*sens_vertical))
+			camera_mount.rotation.x = clamp(camera_mount.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 			# if first.current:
 			#	camera_mount.rotation.x = clamp(camera_mount.rotation.x, -1, 1)
+		else:
+			rotate_y(deg_to_rad(0))
+			rotation.z = 0.0
 
 
 func _physics_process(delta):
+	
 	if Input.is_action_pressed("run"):
 		SPEED = running_speed
 		running = true
@@ -68,7 +93,11 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = lerp(
+		direction, 
+		(transform.basis*Vector3(input_dir.x, 0.01, input_dir.y)).normalized(),
+		delta*lerp_spd+sin(deg_to_rad(delta/2))
+	)
 	if direction:
 		if !is_locked:
 			if running:
@@ -94,6 +123,8 @@ func _physics_process(delta):
 		move_and_slide()
 signal PlayerDie
 func _process(delta):
+	apply_smooth_rotation(delta)
+	z_shaking(delta)
 	if(global_position[1] <=-100) && !was_emit_die:
 		PlayerDie.emit()
 		was_emit_die = true
@@ -110,3 +141,37 @@ func on_Esc(cam):
 func reset():
 	was_emit_die = false
 	self.position = Vector3(0,0,0)
+
+func z_shaking(delta):
+	rotation_speed = clamp(rotation.z, 0.01, 0.05)
+	if is_locked_cam == true:
+		rotation.z = lerp_angle(rotation.z, 0.0, rotation_speed * delta)
+		rotation.z = clamp(rotation.z, deg_to_rad(-2), deg_to_rad(2))
+		current_z_rotation_speed = 0.0
+	else:
+		if target_rotation != prev_target_rotation:
+			current_z_rotation_speed = 0.0
+			rotation.z = 0.0
+		current_z_rotation_speed += (transform.basis*Vector3(direction.x, 0.01, direction.y)).x * z_rotation_speed
+		# if direction.x == 0.0:
+		#	current_z_rotation_speed = lerp(current_z_rotation_speed, 0.01, z_rotation_decay)
+		
+		if (abs(rotation.z) > deg_to_rad(5)) or (abs(rotation.z) < deg_to_rad(-5)):
+			current_z_rotation_speed -= sign(rotation.z) * z_rotation_deceleration * delta
+		current_z_rotation_speed = clamp(current_z_rotation_speed, -max_z_rotation_speed, max_z_rotation_speed)
+		
+		rotation.z += current_z_rotation_speed * delta
+		rotation.z = fmod(rotation.z, (2 * PI))
+		rotation.z = clamp(rotation.z, deg_to_rad(-5), delta*lerp_spd*deg_to_rad(5))
+	prev_target_rotation = target_rotation
+func apply_smooth_rotation(delta):
+	if is_locked_cam:
+		rotation.y = 0.0
+	else:
+		rotation.y = lerp_angle(rotation.y, target_rotation.y, rotation_speed * delta)
+	
+func lerp_angle(a, b, t):
+	var result = a + (b - a) * t
+	if abs(b - a) > PI:
+		result += PI * 2 if (b < a) else -PI * 2
+	return result
